5aaab1928406cc62b949d8f69796ef6296ca031b
Raul Martin <alu0100769579@ull.edu.es>
2017-04-27 17:53:25 +0000

    Añadiendo documentacion

--- triangle.js
+++ triangle.js
@@ -2,10 +2,34 @@
 
 let Shape = require('@alu0100769579/ull-shape-alu0100769579');
 
+/**
+ *Paquete npm de Triangle
+ * 
+ * Autor: Raúl Martín Morales
+ * 
+ * Aquí creamos la clase Triangle que hereda de Shape
+ * 
+ * @class Triangle
+ * @param {hash} options Ancho y Alto
+ * 
+ */
+ 
 class Triangle extends Shape {
+  
+    /**
+    * @construct Triangle
+    * @param{hash} options Ancho y Alto
+    */
+    
     constructor(options) {
       super(options)
     }
+    
+    /**
+    * @function getArea
+    * @return {value} retorna el valor del area de cualquier forma geométrica a la que apunte el objeto
+    */
+    
     area() {
       return 0.5 * this.width * this.height;
     }
